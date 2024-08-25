33efd65925a660b3acf9c9a2233a4c968dbf589f
AriyanSep0_0 <ariansepahi825121314151617@gmail.com>
2024-08-25 20:32:25 +0330

    :tada: Add base style to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "640px",
+        tablet: "1024px",
+        desktop: "1280px",
+      },
+      fontSize: {
+        "2xs": ["0.625rem", "1rem"],
+        heading: ["2rem", "2.25rem"],
+      },
+      colors: {
+        shark: "#1B1D1F",
+        woodsmoke: "#111315",
+        palesky: "#6F757C",
+        surf: "#BEE3CC",
+        bridalhealth: "#FEF7EE",
+        creamcan: "#F6C768",
+        terracotta: "#ED735D",
+      },
+    },
   },
   plugins: [],
 };
